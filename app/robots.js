b3ed76d6f4a9d574aef75e7d7ed7c2475f283409
j1ngzoue <workdrive0306@gmail.com>
2026-04-16 19:09:54 +0900

    feat(seo): add robots.txt and sitemap.xml via App Router

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://3d-thumbnail-generator.vercel.app/sitemap.xml',
+  }
+}
---
+++ app/sitemap.js
@@ -0,0 +1,9 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://3d-thumbnail-generator.vercel.app',
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+  ]
+}
